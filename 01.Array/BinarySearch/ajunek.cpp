29222ad5912bfb4aed7374ac4b3b9e8049603799
Amol Shinde <shindeamol409@gmail.com>
2023-03-02 18:14:05 +0530

    Added new files.

---
+++ 01.Array/BinarySearch/ajunek.cpp
@@ -0,0 +1,106 @@
+/*#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int key) {
+    int start = 0;
+    int end = size - 1;
+    int mid = start + (end-start)/2;
+
+    while(start <= end) {
+        if (arr[mid] == key) {
+            
+            return mid;
+        }
+        if (key > arr[mid]) {
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+        mid = start + (end-start)/2;
+    }
+    return -1;
+}
+
+int main() {
+    
+    int even [6] = {3, 6, 9, 12, 15, 18};
+    int odd [5] = {9, 18, 27, 36, 45};
+
+    int evenIndex = binarySearch(even, 6, 12);
+    cout << "Index of 12 is: " << evenIndex << '\n';
+
+    int oddIndex = binarySearch(odd, 5, 45);
+    cout << "Index of 45 is: " << oddIndex << '\n';
+    return 0;
+}*/
+/*
+#include <iostream>
+using namespace std;
+int binarySearch(int arr[], int n, int key) {
+    int start = 0;
+    int end = n-1;
+    int mid = start + (end-start)/2;
+    while (start <= end) {
+        if (arr[mid] == key) {
+            return mid;
+        }
+        if (key > arr[mid]) {
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+        mid = start + (end-start)/2;
+    }
+    return -1;
+
+}
+
+int main() {
+    int even [6] = {2,4,6,8,12,18};
+    int odd [5] = {3, 8, 11, 14, 16}; 
+
+    int evenIndex = binarySearch(even, 6, 8);
+    cout << "Index of 8 is: " << evenIndex << '\n';
+
+    int oddIndex = binarySearch(odd, 5, 14);
+    cout << "Index of 14 is: " << oddIndex << '\n';
+    return 0;
+}*/
+
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int key) {
+
+    int start = 0;
+    int end = n-1;
+    int mid = start + (end-start)/2;
+
+    while(start <= end) {
+        if (arr[mid] == key) {
+            return mid;
+        }
+        if(key > arr[mid]) {
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+        mid = start + (end-start)/2;
+    }
+    return -1;
+}
+
+int main() {
+    int even [6] = {3, 6, 9, 12, 15, 18};
+    int odd [5] = {9, 18, 27, 36, 45};
+
+    int evenIndex = binarySearch(even, 6, 15);
+    cout << "Index of 15 is: " << evenIndex << '\n';
+
+    int oddIndex = binarySearch(odd, 5, 36);
+    cout << "Index of 36 is: " << oddIndex << '\n';
+    return 0;
+}
