36385335f0810fd7ab489dc6a49ebf2b7404654d
Amol Shinde <shindeamol409@gmail.com>
2023-03-13 15:36:43 +0530

    Added new files.

--- 01.Array/SqrtWithMorePrecision/ajunek.cpp
+++ 01.Array/SqrtWithMorePrecision/ajunek.cpp
@@ -2,27 +2,26 @@
 using namespace std;
 
 long long int sqrtInteger(int n) {
-
     int start = 0;
     int end = n;
-    long long int mid = start + (end - start)/2;
+    long long mid = start + (end - start)/2;
 
     long long int ans = -1;
-    while(start <= end) {
+    while (start <= end) {
 
         long long int square = mid * mid;
-
+        
         if (square == n)
-         return mid;
+              return mid;
 
-        if(square < n) {
-            ans = mid;
-            start = mid + 1;
-        }
-        else {
-            end = mid - 1;
-        }
-        mid = start + (end - start)/2;  
+              if (square < n) {
+                ans = mid;
+                start = mid + 1;
+              }
+              else {
+                end = mid - 1;
+              }
+              mid = start + (end - start)/2;
     }
     return ans;
 }
@@ -35,7 +34,7 @@ double morePrecision(int n, int precision, int tempSol) {
     for(int i = 0; i < precision; i++) {
         factor = factor/10;
 
-        for(double j = ans; j*j < n; j = j+factor) {
+        for (double j = ans; j*j < n; j = j + factor) {
             ans = j;
         }
     }
@@ -44,11 +43,177 @@ double morePrecision(int n, int precision, int tempSol) {
 
 int main() {
     int n;
-    cout << "Enter the number " << endl;
+    cout << "Enter the number: " << '\n';
     cin >> n;
 
     int tempSol = sqrtInteger(n);
-    cout << "Answer is: " << morePrecision(n, 3, tempSol) << endl;
+    cout << "Answer is " << morePrecision(n, 3, tempSol) << '\n';
+
+    return 0;
+}
+
+/*------------------------------------------------------------------------------*/
+
+#include <iostream>
+using namespace std;
+
+long long int sqrtInteger(int n) {
+
+    int start = 0;
+    int end = n;
+    long long int mid = start + (end - start)/2;
+    long long int ans = -1;
+
+    while (start <= end) {
+
+        long long int square = mid * mid;
+        
+        if (square == n)
+             return mid;
+
+        if (square < n) {
+            ans = mid;
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+        mid = start + (end - start)/2;     
+    }
+
+    return ans;
+}
+
+double morePrecision(int n, int precision, int tempSol) {
+
+    double factor = 1;
+    double ans = tempSol;
+
+    for (int i = 0; i < precision; i++) {
+        factor = factor/10;
+
+        for (double j = ans; j*j < n; j = j+factor ) {
+            ans = j;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number: " << '\n';
+    cin >> n;
+
+    int tempSol = sqrtInteger(n);
+    cout << "Answer is: " << morePrecision(n, 3, tempSol) << '\n';
+
+    return 0;
+}
+
+/*-------------------------------------------------------------------------*/
+#include <iostream>
+using namespace std;
+
+long long int sqrtInteger(int n) {
+
+    int start = 0;
+    int end = n;
+    long long int mid = start + (end - start)/2;
+    long long int ans = -1;
+
+    while (start <=  end) {
+        long long int square = mid*mid;
+        
+        if (square == n) {
+            return mid;
+        }
+        if (square < n) {
+            ans = mid;
+            start = mid + 1;
+        }
+        else {
+            end = mid -1;
+        }
+        mid = start + (end - start)/2;
+    }
+    return ans;
+}
+double morePrecision(int n, int precision, int tempSol) {
+
+    double factor = 1;
+    double ans = tempSol;
+
+    for (int i = 0; i < precision; i++) {
+        factor = factor/10;
+
+        for (double j = ans; j*j < n; j = j+factor) {
+            ans = j;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number: " << '\n';
+    cin >> n;
+
+    int tempSol = sqrtInteger(n);
+    cout << "Answer is " << morePrecision(n, 3, tempSol) << '\n';
+
+    return 0;
+}
+
+/*-----------------------------------------------------------------------------*/
+
+#include <iostream>
+using namespace std;
+
+long long int sqrtInteger(int n) {
+
+    int start = 0;
+    int end = n;
+    long long int mid = start + (end - start)/2;
+    long long int ans = -1;
+
+    while (start <= end) {
+        long long int square = mid*mid;
+
+        if (square == n) {
+            return mid;
+        } 
+        if (square < n) {
+            ans = mid;
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+        mid = start + (end - start)/2;
+    }
+    return 0;
+}
+double morePrecision(int n, int precision, int tempSol) {
+
+    double factor = 1;
+    double ans = tempSol;
+
+    for (int i = 0; i < precision; i++) {
+        factor = factor/10;
+
+        for(double j = ans; j*j < n; j = j+factor) {
+            ans = j;
+        }
+    }
+    return ans;
+}
+int main() {
+    int n;
+    cout << "Enter the number: " << '\n';
+    cin >> n;
+
+    int tempSol = sqrtInteger(n);
+    cout << "Answer is " << morePrecision(n, 3, tempSol) << '\n';
 
     return 0;
 }
