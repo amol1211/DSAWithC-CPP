aca08705b159c833bac24f2abd5d166944cca8a0
Amol Shinde <shindeamol409@gmail.com>
2023-03-16 19:04:47 +0530

    Added new files.

--- 02.SolvingLeetcodeArray/BookAllocationProblem(CodesStudio)/ajunek.cpp
+++ 02.SolvingLeetcodeArray/BookAllocationProblem(CodesStudio)/ajunek.cpp
@@ -252,3 +252,107 @@ int alloacateBooks(vector<int> arr, int n, int m) {
     }
     return ans;
 }
+
+/*----------------------------------------------------------------------------------------*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isPossible(vector<int> arr, int n, int m, int mid) {
+    int studentCount = 1;
+    int pageSum = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (pageSum + arr[i] <= mid) {
+            pageSum += arr[i];
+        }
+        else {
+            studentCount++;
+            if(studentCount > m || arr[i] > mid) {
+                return false;
+            }
+            pageSum = arr[i];
+        }
+        if(studentCount > m) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int allocateBooks(vector<int> arr, int n, int m) {
+    int start = 0;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    int end = sum;
+    int ans = -1;
+    int mid = start + (end - start)/2;
+
+    while (start <= end) {
+
+        if(isPossible(arr,n,m,mid)) {
+
+            ans = mid;
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+        mid = start + (end - start)/2;
+    }
+    return ans;
+}
+
+/*--------------------------------------------------------------------------------------*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isPossible(vector<int> arr, int n, int m, int mid) {
+    int studentCount = 1;
+    int pageSum = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (pageSum + arr[i] <= mid) {
+            pageSum += arr[i];
+        }
+        else {
+            studentCount++;
+            if(studentCount > m || arr[i] > mid) {
+                return false;
+            }
+            pageSum = arr[i];
+        }
+        if(studentCount > m) {
+            return false;
+        }
+    }
+    return true;
+
+}
+int allocateBooks(vector<int> arr, int n, int m) {
+    int start = 0;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    int end = sum;
+    int ans = -1;
+    int mid = start + (end - start)/2;
+
+    while (start <= end) {
+        if (isPossible(arr,n,m,mid)) {
+            ans = mid;
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+        mid = start + (end - start)/2;
+    }
+    return ans;
+}
