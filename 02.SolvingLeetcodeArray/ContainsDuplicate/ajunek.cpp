bbf55adf241da295ef36fe7a67c656f4c0b2c612
Amol Shinde <shindeamol409@gmail.com>
2023-03-18 23:46:20 +0530

    Added new files.

--- 02.SolvingLeetcodeArray/ContainsDuplicate/ajunek.cpp
+++ 02.SolvingLeetcodeArray/ContainsDuplicate/ajunek.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
@@ -10,4 +14,24 @@ public:
         return false;
     }
 };
+/*------------------------------------------------------------*/
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> count_map;
+        for (int i = 0; i < nums.size(); i++) {
+            count_map[nums[i]]++;
+            if(count_map[nums[i]] > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+        
     
