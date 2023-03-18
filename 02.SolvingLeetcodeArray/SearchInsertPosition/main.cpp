91e5073688e0d084e5f32671663ec06dba2fab09
Amol Shinde <shindeamol409@gmail.com>
2023-03-19 00:41:13 +0530

    Added new files.

--- 02.SolvingLeetcodeArray/SearchInsertPosition/main.cpp
+++ 02.SolvingLeetcodeArray/SearchInsertPosition/main.cpp
@@ -24,18 +24,14 @@ int searchInsert(vector<int>& nums, int target) {
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        /// return index of first one that comp(item,target)==true, or nums.size() if not found
-		/// comp is greater or equal to for lower_bound
-		/// comp is greater for upper_bound
         int first=0, last=nums.size(), mid;
         while (first<last) {
-            mid=first+((last-first)>>1); // first<=mid, mid<last
-			/// if comp(item,target)==false, advance first
-			// if(nums[mid]<=target) // for upper_bound
-			if (nums[mid]<target) // for lower_bound
-				first=mid+1; // first always increases
-			else /// else recede last
-				last=mid; // last always decreases (even last-first==1)
+            mid=first+((last-first)/2); 
+            	
+			if (nums[mid]<target) 
+				first=mid+1;
+			else 
+				last=mid;
         }
         return first;
     }
