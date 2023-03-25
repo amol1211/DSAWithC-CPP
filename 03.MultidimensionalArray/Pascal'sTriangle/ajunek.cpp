1faf3f39e925e676bfdab114d15a565c045420c5
Amol Shinde <shindeamol409@gmail.com>
2023-03-26 01:38:19 +0530

    Added new files.

--- 03.MultidimensionalArray/Pascal'sTriangle/ajunek.cpp
+++ 03.MultidimensionalArray/Pascal'sTriangle/ajunek.cpp
@@ -31,8 +31,8 @@ int main() {
 
     for(int i = 0; i < ans.size(); i++) {
         for(int j = 0; j < ans[i].size(); j++) {
-            cout << ans[i][j] <<" ";
+            cout << ans[i][j]<<" ";
         }
-        cout << '\n';
+        cout <<'\n';
     }
 }
