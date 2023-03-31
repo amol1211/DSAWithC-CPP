cd27e834f38464c770e0a84e761ad6c4eeeb714e
Amol Shinde <shindeamol409@gmail.com>
2023-03-31 23:26:47 +0530

    Added new files.

--- 03.MultidimensionalArray/ReturnElementsOfMatrixInSpiralOrder/ajunek.cpp
+++ 03.MultidimensionalArray/ReturnElementsOfMatrixInSpiralOrder/ajunek.cpp
@@ -2,15 +2,7 @@
 #include <vector>
 using namespace std;
 
-// 1 2 3
-// 4 5 6
-// 7 8 9
 
-//left = 1, right = 1
-//top = 2, bottom = 1
-//direction = 3 + 1 % 4 = 0
-
-// 1 2 3 6 9 8 7 4 5
 void spiralOrder(vector<vector<int>>& matrix) {
 
     int left = 0;
@@ -21,15 +13,15 @@ void spiralOrder(vector<vector<int>>& matrix) {
     int direction = 0;
 
     while(left <= right && top <= bottom) {
-        // left -> right
+
         if(direction == 0) {
             for(int col = left; col <= right; col++) {
-            cout << matrix[top][col] <<" ";
+                cout << matrix[top][col] <<" ";
             }
             top++;
         }
 
-        // top -> bottom
+
         else if(direction == 1) {
             for(int row = top; row <= bottom; row++) {
                 cout << matrix[row][right] <<" ";
@@ -37,7 +29,6 @@ void spiralOrder(vector<vector<int>>& matrix) {
             right--;
         }
 
-        //right -> left
         else if(direction == 2) {
             for(int col = right; col >= left; col--) {
                 cout << matrix[bottom][col] <<" ";
@@ -45,27 +36,26 @@ void spiralOrder(vector<vector<int>>& matrix) {
             bottom--;
         }
 
-        // bottom -> top
         else {
             for(int row = bottom; row >= top; row--) {
                 cout << matrix[row][left] <<" ";
             }
             left++;
         }
-        direction = (direction + 1)%4; //0,1,2,3
+        direction = (direction + 1) % 4;
 
     }
 }
 
 int main() {
 
-    int n,m;
+    int n, m;
     cin>>n>>m;
     vector<vector<int>> matrix(n, vector<int> (m));
 
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            cin>>matrix[i][j];
+            cin >> matrix[i][j];
         }
     }
 
@@ -73,5 +63,67 @@ int main() {
 
     return 0;
 }
-/*------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------------------------------*/
 #include <iostream>
+#include <vector>
+using namespace std;
+
+void spiralOrder(vector<vector<int>>& matrix) {
+
+    int left = 0;
+    int right = matrix[0].size() - 1;
+    int top = 0;
+    int bottom = matrix.size() - 1;
+
+    int direction = 0;
+
+    while(left <= right && top <= bottom) {
+
+        if(direction == 0) {
+            for(int col = left; col <= right; col++) {
+                cout << matrix[top][col] <<" ";
+            }
+            top++;
+        }
+
+        else if(direction == 1) {
+            for(int row = top; row <= bottom; row++) {
+                cout << matrix[row][right] <<" ";
+            }
+            right--;
+        }
+
+        else if(direction == 2) {
+            for(int col = right; col >= left; col--) {
+                cout << matrix[bottom][col] <<" ";
+            }
+            bottom--;
+        }
+        else {
+            for(int row = bottom; row >= top; row--) {
+                cout << matrix[row][left]<<" ";
+            }
+            left++;
+        }
+        direction = (direction + 1) % 4;
+    }
+}
+
+int main() {
+
+    int n,m;
+    cin>>n>>m;
+
+    vector<vector<int>> matrix(n, vector<int> (m));
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    spiralOrder(matrix);
+
+    return 0;
+}
