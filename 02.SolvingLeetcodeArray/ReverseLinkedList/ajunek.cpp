d97daa5b61839a423db8ffbfa28be1c96f95a6bf
Amol Shinde <shindeamol409@gmail.com>
2023-04-05 17:55:31 +0530

    Added new files

--- 02.SolvingLeetcodeArray/ReverseLinkedList/ajunek.cpp
+++ 02.SolvingLeetcodeArray/ReverseLinkedList/ajunek.cpp
@@ -45,3 +45,18 @@ public:
         return prevNode;
     }
 };
+
+/*-----------------------------------------------------*/
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *nextNode, *prevNode = NULL;
+        while (head) {
+            nextNode = head->next;
+            head->next = prevNode;
+            prevNode = head;
+            head = nextNode;
+        }
+        return prevNode;
+    }
+};
