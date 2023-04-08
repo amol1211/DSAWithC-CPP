742a805c128fc2d36ba80a0ac5816313ebccf022
Amol Shinde <shindeamol409@gmail.com>
2023-04-08 23:06:23 +0530

    Added new files.

--- 02.SolvingLeetcodeArray/ValidPalindrome/main.cpp
+++ 02.SolvingLeetcodeArray/ValidPalindrome/main.cpp
@@ -18,3 +18,21 @@ public:
 	return true;
     }
 };
+
+/*--------------------------------------------------------------------*/
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start=0, end=s.length()-1;
+	    while(start<end) {
+		if (!isalnum(s[start])) start++;
+		else if (!isalnum(s[end])) end--;
+		else {
+			if (tolower(s[start++])!=tolower(s[end--]))
+                return false;
+		}
+	}
+	return true;
+    }
+};
