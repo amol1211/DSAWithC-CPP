4592755353e1c62f1c608bbcbb9ee20c04ec76e8
Amol Shinde <shindeamol409@gmail.com>
2023-04-19 00:15:14 +0530

    Added new files.

--- 01.LearningTheBasic/01.1PatternProblems/Pattern1/ajunek.cpp
+++ 01.LearningTheBasic/01.1PatternProblems/Pattern1/ajunek.cpp
@@ -30,3 +30,36 @@ int main()
 
     return 0;
 }
+/*---------------------------------------------------------------*/
+#include <iostream>
+using namespace std;
+
+void pattern1(int n)
+{
+    // This is the outer loop which will loop for the rows.
+    for (int i = 0; i < n; i++)
+    {
+        // This is the inner loop which here, loops for the columns
+        // as we have to print a rectangular pattern.
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+        }
+       
+        // As soon as N stars are printed, we move to the
+        // next row and give a line break otherwise all stars
+        // would get printed in 1 line.
+        cout << endl;
+    }
+}
+
+int main()
+{   
+    // Here, we have taken the value of N as 5.
+    // We can also take input from the user.
+    int n = 5;
+
+    pattern1(n);
+
+    return 0;
+}
