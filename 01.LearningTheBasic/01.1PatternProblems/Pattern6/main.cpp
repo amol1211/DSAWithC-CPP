62d0ad9aec375fd5dd3c4f92beb7c9174f24d5de
Amol Shinde <shindeamol409@gmail.com>
2023-04-25 22:55:36 +0530

    Added new files.

--- 01.LearningTheBasic/01.1PatternProblems/Pattern6/main.cpp
+++ 01.LearningTheBasic/01.1PatternProblems/Pattern6/main.cpp
@@ -15,7 +15,7 @@ void pattern6(int n)
     // This is the outer loop which will loop for the rows.
     for (int i = 0; i < n; i++)
     {
-         // This is the inner loop which loops for the columns
+        // This is the inner loop which loops for the columns
         // no. of columns = (N - row index) for each line here
         // as we have to print an inverted pyramid.
         // (N-j) will give us the numbers in a row starting from 1 to N-i.
@@ -35,6 +35,31 @@ int main()
 {   
     // Here, we have taken the value of N as 5.
     // We can also take input from the user.
+    int n = 5;
+
+    pattern6(n);
+
+    return 0;
+}
+
+/*------------------------------------------------------------*/
+
+#include <iostream>
+using namespace std;
+
+void pattern6(int n) {
+
+    for (int i = 0; i < n; i++) {
+        for (int j = n; j > i; j--) {
+            cout << n - j + 1 << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+int main () {
+
     int n = 5;
 
     pattern6(n);
