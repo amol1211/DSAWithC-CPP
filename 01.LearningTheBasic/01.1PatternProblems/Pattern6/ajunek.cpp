89be2c0e819a7baf61906225cb1f224ce3621019
Amol Shinde <shindeamol409@gmail.com>
2023-04-26 22:19:33 +0530

    Added new files.

--- 01.LearningTheBasic/01.1PatternProblems/Pattern6/ajunek.cpp
+++ 01.LearningTheBasic/01.1PatternProblems/Pattern6/ajunek.cpp
@@ -22,26 +22,3 @@ int main () {
 }
 
 /*--------------------------------------------------*/
-
-#include <iostream>
-using namespace std;
-
-void pattern6(int n) {
-
-    for (int i = 0; i < n; i++) {
-        for (int j = n; j > i; j--) {
-            cout << n - j + 1 << " ";
-        }
-
-        cout << endl;
-    }
-}
-
-int main () {
-
-    int n = 5;
-
-    pattern6(n);
-
-    return 0;
-}
