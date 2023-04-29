48b8349132842e0f1e63dc18c4815f6aea59deff
Amol Shinde <shindeamol409@gmail.com>
2023-04-30 02:56:07 +0530

    Added new files.

--- 03.SolvingLeetcodeArray/PerfectNumber/ajunek.cpp
+++ 03.SolvingLeetcodeArray/PerfectNumber/ajunek.cpp
@@ -13,3 +13,24 @@ class Solution {
     return sum == num;
     }
 };
+
+/*------------------------------------------------*/
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        int sum = 1;
+        int sqrt_num = sqrt(num);
+        for (int i = 2; i <= sqrt_num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if ( i != num/i) {
+                    sum += num/i;
+                }
+            }
+        }
+        return sum == num;
+    }
+};
--- 03.SolvingLeetcodeArray/PerfectNumber/main.cpp
+++ 03.SolvingLeetcodeArray/PerfectNumber/main.cpp
@@ -23,3 +23,25 @@ int main() {
     cout << s.checkPerfectNumber(num) << '\n';
     return 0;
 }
+
+/*--------------------------------------------------------*/
+
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        int sum = 1;
+        int sqrt_num = sqrt(num);
+        for (int i = 2; i <= sqrt_num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if ( i != num/i) {
+                    sum += num/i;
+                }
+            }
+        }
+        return sum == num;
+    }
+};
