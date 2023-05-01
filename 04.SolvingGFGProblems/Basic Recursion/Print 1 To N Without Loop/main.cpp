7dc368e39824425b852deaf47a24f604a5b9f7a1
Amol Shinde <shindeamol409@gmail.com>
2023-05-01 19:27:02 +0530

    Added new files.

---
+++ 04.SolvingGFGProblems/Basic Recursion/Print 1 To N Without Loop/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class Solution{
+    public:
+    void printNos(int N) {
+    if (N > 1) {
+        printNos(N-1);
+    }
+    cout << N << " ";
+    
+    }
+};
