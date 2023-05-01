c4692d6850c99aa77c5eaa23b7715bfb34578696
Amol Shinde <shindeamol409@gmail.com>
2023-05-01 19:50:00 +0530

    Added new files.

---
+++ 04.SolvingGFGProblems/Basic Recursion/Print GFG n times/ajunek.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+    public:
+    void printGfg(int N) {
+        if (N > 1) {
+            printGfg(N - 1);
+        }
+        cout << "GFG ";
+    }
+};
