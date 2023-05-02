1d2bf723e93b8258ce0f2b338e1edb0f6323488b
Amol Shinde <shindeamol409@gmail.com>
2023-05-02 23:25:55 +0530

    Added new files

---
+++ 04.SolvingGFGProblems/Basic Recursion/Find all factorial numbers less than or equal to N/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+
+    vector<long long> factorialNumbers(long long n)
+    {
+        // Write Your Code here
+       vector< long long > ans = {1};
+       for( int i = 1; i<= n ; i++ ){
+           
+           long long t = i * ans[i-1] ; 
+           if( t > n)
+             break;
+           
+           ans.push_back( t );
+       }
+       
+           auto it = ans.begin();
+       ans.erase(it);
+       
+       return ans;
+      
+    }
+};
+
+/*----------------OR-----------------------------------------*/
+
+//User function Template for C++
+//User function Template for C++
+class Solution
+{
+public:
+    vector<long long> factorialNumbers(long long N)
+    {
+        
+        vector<long long> v;
+        
+        long long mul=1;
+        
+        for(int i=1;mul*i<=N;i++)
+        {
+            mul*=i;
+            v.push_back(mul);
+        }
+        
+        return v;
+    }
+};
