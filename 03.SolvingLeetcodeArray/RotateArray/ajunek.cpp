ec5053773706fa3c422d47453347c3ceeb34d38a
Amol Shinde <shindeamol409@gmail.com>
2023-05-06 23:29:04 +0530

    Added new files.

--- 03.SolvingLeetcodeArray/RotateArray/ajunek.cpp
+++ 03.SolvingLeetcodeArray/RotateArray/ajunek.cpp
@@ -15,3 +15,23 @@ class Solution {
         }
     }
 };
+
+/*------------------------------------------------------*/
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n; // calculate the actual number of rotations needed
+        vector<int> temp(k); // create an extra array to store the rotated elements
+        for (int i = n - k; i < n; i++) { // copy the last k elements to the beginning of the extra array
+            temp[i - (n - k)] = nums[i];
+        }
+        for (int i = n - k - 1; i >= 0; i--) { // shift the remaining elements to the right
+            nums[i + k] = nums[i];
+        }
+        for (int i = 0; i < k; i++) { // copy the rotated elements from the extra array to the beginning of the original array
+            nums[i] = temp[i];
+        }
+    }
+};
