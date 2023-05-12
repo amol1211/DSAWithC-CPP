e7a8a9607cc5d3c94b645e5c069b6066f416f2f0
Amol Shinde <shindeamol409@gmail.com>
2023-05-13 02:12:46 +0530

    Added new files.

--- 03.SolvingLeetcodeArray/FibonacciNumber/ajunek.cpp
+++ 03.SolvingLeetcodeArray/FibonacciNumber/ajunek.cpp
@@ -28,3 +28,12 @@ class Solution {
 };
 
 /*-------------------------------------------------------*/
+
+class Solution {
+    public:
+    int fib(int n) {
+        double sqrt5 = sqrt(5);
+        return (pow(1 + sqrt5, n) - pow(1 - sqrt5, n)) / pow(2, n) / sqrt5;
+
+    }
+};
