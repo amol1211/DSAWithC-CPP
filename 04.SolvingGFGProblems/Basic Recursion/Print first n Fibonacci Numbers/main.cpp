493653ecd1338135fa5ed823605cbcbcbf5a97ec
Amol Shinde <shindeamol409@gmail.com>
2023-05-12 18:36:45 +0530

    Added new files.

--- 04.SolvingGFGProblems/Basic Recursion/Print first n Fibonacci Numbers/main.cpp
+++ 04.SolvingGFGProblems/Basic Recursion/Print first n Fibonacci Numbers/main.cpp
@@ -1,29 +1,15 @@
 #include <iostream>
 using namespace std;
 
-class Solution
-{
-    public:
-    //Function to return list containing first n fibonacci numbers.
-    vector<long long> printFibb(int n) 
-    {   
-        vector<long long> v;
-        for (int i = 0; i < n; i++) {
-            v.push_back(fibonacci(i));
+class Solution {
+public:
+    vector<long long> printFibb(int N) {
+        vector<long long> fib(N); // create a vector to store the Fibonacci numbers
+        fib[0] = 1; // set the first two numbers to 1
+        fib[1] = 1;
+        for (int i = 2; i < N; i++) {
+            fib[i] = fib[i-1] + fib[i-2]; // compute the i-th Fibonacci number
         }
-        return v;
-    }
-    
-    //Function to calculate the nth Fibonacci number.
-    long long fibonacci(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        else if (n == 1) {
-            return 1;
-        }
-        else {
-            return fibonacci(n-1) + fibonacci(n-2);
-        }
+        return fib; // return the vector of Fibonacci numbers
     }
 };
