b142af07871c822f0fafd1fe9073546605ab3d50
Amol Shinde <shindeamol409@gmail.com>
2023-05-15 04:24:29 +0530

    Added new files.

--- 04.SolvingGFGProblems/Basic Hashing/Frequencies of Limited Range Array Elements/main.cpp
+++ 04.SolvingGFGProblems/Basic Hashing/Frequencies of Limited Range Array Elements/main.cpp
@@ -1,46 +1,54 @@
 #include <iostream>
 using namespace std;
 
-class Solution {
-    public:
-    void frequencyCount(vector<int>& arr, int N, int P) {
-    // iterate through the array and update its values
-    for (int i = 0; i < N; i++) {
-        // Calculate the index of the current element
-        int index = arr[i] - 1;
-
-        // We add P to the value at index (arr[i] - 1) % P to keep track of 
-        // how many times the number has occurred in the array.
-        // The modulo operator is used to ensure that the index is within the range [0, P-1].
-        arr[index % N] += N;
-    }
-
-    // iterate through the array again and compute the frequency count
-    for (int i = 0; i < N; i++) {
-        // Divide each value by N to obtain the frequency count of each element.
-        arr[i] /= N;
-    }
-}
-
-};
-
-/*------------------------------------------------------------------------------------*/
+// Using unordered_map
 
 class Solution{
     public:
+    //Function to count the frequency of all elements from 1 to N in the array.
     void frequencyCount(vector<int>& arr,int N, int P)
     { 
-        // Step 1: Initialize the frequency array
+        unordered_map<int, int> freq;
+    
+    // iterate over the input array and update frequency count
+    for(int i=0;i<N;i++){
+        freq[arr[i]]++;
+    }
+    
+    // iterate through the frequency table and update the input array
+    for(int i=1;i<=N;i++){
+        arr[i-1] = freq[i];
+    }
+    }
+};
+
+//Time complexity : O(N)
+//Space complexity : O(N)
+
+/*-------------------------------------------------------------------------------------*/
+
+//2. Using the counting sort algorithm to count the frequency of elements
+class Solution {
+public:
+    // Function to count the frequency of all elements from 1 to N in the array.
+    void frequencyCount(vector<int>& arr, int N, int P) {
+        // Create a frequency array of size N with all elements initialized to 0.
         vector<int> freq(N, 0);
-        
-        // Step 2: Update the frequency array
-        for(int i = 0; i < arr.size(); i++){
+
+        // Iterate over the input array and update frequency count for valid elements.
+        for(int i = 0; i < arr.size(); i++) {
+            // If the element is greater than N or less than 1, skip it.
+            if(arr[i] > N || arr[i] < 1) continue;
+            // Increment the count for this element in the frequency array.
             freq[arr[i] - 1]++;
         }
-        
-        // Step 3: Copy the values from the frequency array to the input array
-        for(int i = 0; i < N; i++){
+
+        // Update the input array with the frequency counts.
+        for (int i = 0; i < N; i++) {
             arr[i] = freq[i];
         }
     }
 };
+
+//Time complexity : O(n + N
+//Space complexity : O(N)
