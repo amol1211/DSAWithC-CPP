e436be2ff4dc5bb30b1d709670df67afeb6f289f
Amol Shinde <shindeamol409@gmail.com>
2023-05-15 07:44:23 +0530

    Added new files.

--- 04.SolvingGFGProblems/Basic Hashing/Top K Frequent Elements in Array/ajunek.cpp
+++ 04.SolvingGFGProblems/Basic Hashing/Top K Frequent Elements in Array/ajunek.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
 using namespace std;
 
+class Solution {
+    public:
+    vector<int> topK(vector<int>& nums, int k) {
+
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        priority_queue<pair<int, int>> pq;
+        for (auto it : freq) {
+            pq.push({it.second, it.first});
+        }
+
+        vector<int> res;
+        while (k-- > 0 && !pq.empty()) {
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return res;
+    }
+};
