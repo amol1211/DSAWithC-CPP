2dd0cb26d2d78da19e1b80d8fdf33161da1da441
Amol Shinde <shindeamol409@gmail.com>
2023-05-20 23:46:51 +0530

    Added new files.

--- 03.SolvingLeetcodeProblems/TwoSum/ajunek.cpp
+++ 03.SolvingLeetcodeProblems/TwoSum/ajunek.cpp
@@ -40,3 +40,32 @@ class solution {
     }
 };
 
+/*--------------------------------------------------------------------------------------------------------------*/
+//3. Using Hashmap
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // Create a hashmap to store the element-value pairs
+        unordered_map<int, int> hashmap;
+        
+        // Iterate through the input array
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
+            int complement = target - num;
+            
+            // Check if the complement exists in the hashmap
+            if (hashmap.count(complement)) {
+                // If it does, then return the current index and the complement's index
+                return {hashmap[complement], i};
+            }
+            
+            // Otherwise, add the current element and its index to the hashmap
+            hashmap[num] = i;
+        }
+        
+        // If no two elements add up to the target, then return an empty vector
+        return {};
+    }
+};
+
