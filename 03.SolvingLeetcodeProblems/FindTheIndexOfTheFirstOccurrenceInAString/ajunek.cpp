773b8aa56d3676457ed6522c0b02290f8548e008
Amol Shinde <shindeamol409@gmail.com>
2023-05-28 23:45:58 +0530

    Added new files.

--- 03.SolvingLeetcodeProblems/FindTheIndexOfTheFirstOccurrenceInAString/ajunek.cpp
+++ 03.SolvingLeetcodeProblems/FindTheIndexOfTheFirstOccurrenceInAString/ajunek.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int haystackLen = haystack.length();
+        int needleLen = needle.length();
+        
+        if (needleLen == 0)
+            return 0;  // Edge case: empty needle, it is considered to be found at index 0.
+        
+        for (int i = 0; i <= haystackLen - needleLen; i++) {
+            int j;
+            for (j = 0; j < needleLen; j++) {
+                if (haystack[i + j] != needle[j])
+                    break;  // Mismatch found, break inner loop.
+            }
+            if (j == needleLen)
+                return i;  // Found needle at index i.
+        }
+        
+        return -1;  // Needle not found in haystack.
+    }
+};
