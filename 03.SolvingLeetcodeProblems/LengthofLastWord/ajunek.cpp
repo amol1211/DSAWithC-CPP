9888fbf7821a2121d597770132015dfff0ca96aa
Amol Shinde <shindeamol409@gmail.com>
2023-05-30 21:55:45 +0530

    Added new files.

--- 03.SolvingLeetcodeProblems/LengthofLastWord/ajunek.cpp
+++ 03.SolvingLeetcodeProblems/LengthofLastWord/ajunek.cpp
@@ -1,15 +1,14 @@
 class Solution {
-public:
+    public:
     int lengthOfLastWord(string s) {
         int n = s.size();
         int count = 0;
-
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = n - 1; i >= n; i--) {
             if (s[i] != ' ') {
-            count++;
+                count++;
             }
             else if (count != 0 && s[i] == ' ') {
-            return count;
+                return count;
             }
         }
         return count;
