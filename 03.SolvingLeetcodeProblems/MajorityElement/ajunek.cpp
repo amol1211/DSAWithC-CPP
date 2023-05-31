13cbbecd3b8732e63c17fb1e434e36c3f851198a
Amol Shinde <shindeamol409@gmail.com>
2023-05-31 17:27:09 +0530

    Added new files.

--- 03.SolvingLeetcodeProblems/MajorityElement/ajunek.cpp
+++ 03.SolvingLeetcodeProblems/MajorityElement/ajunek.cpp
@@ -23,3 +23,15 @@ class Solution {
         return majority;
     }
 };
+
+/*-------------------------------------------------------------*/
+
+class Solution {
+    public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
+    }
+};
+
+/*-------------------------------------------------------------*/
