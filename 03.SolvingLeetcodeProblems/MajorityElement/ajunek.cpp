48e899bf0315bf05f4d15af59a757700863e5c25
Amol Shinde <shindeamol409@gmail.com>
2023-05-31 17:33:27 +0530

    Added new files.

--- 03.SolvingLeetcodeProblems/MajorityElement/ajunek.cpp
+++ 03.SolvingLeetcodeProblems/MajorityElement/ajunek.cpp
@@ -35,3 +35,19 @@ class Solution {
 };
 
 /*-------------------------------------------------------------*/
+
+class Solution {
+    public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        int n = nums.size();
+
+        for (int i : nums) {
+            freq[i]++;
+            if (freq[i] > n/2) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
