cec054c40680c40cb4816332e679512ed21ade7b
Amol Shinde <shindeamol409@gmail.com>
2023-06-02 23:05:01 +0530

    Added new files.

---
+++ 03.SolvingLeetcodeProblems/RemoveDuplicatesFromSortedArray II/main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        int count = 0;
+
+        for (auto i : nums) {
+            freq[i]++;
+            if (freq[i] <= 2) {
+               nums[count++] = i;
+            }
+        }
+        return count;
+    }
+};
