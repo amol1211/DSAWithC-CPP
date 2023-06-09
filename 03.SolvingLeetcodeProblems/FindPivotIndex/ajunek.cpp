1613f213c3243dc65f2c935bb692f965621a8341
Amol Shinde <shindeamol409@gmail.com>
2023-06-09 23:43:10 +0530

    Added new files.

--- 03.SolvingLeetcodeProblems/FindPivotIndex/ajunek.cpp
+++ 03.SolvingLeetcodeProblems/FindPivotIndex/ajunek.cpp
@@ -24,3 +24,16 @@ class solution {
 
 /*-------------------------------------------------------------------------*/
 
+class Solution {
+    public: 
+    int pivotIndex(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            int left = accumulate(nums.begin(), nums.begin()+i,0);
+            int right = accumulate(nums.begin()+i+1, nums.end(),0);
+
+            if (left == right) 
+            return i;
+        }
+        return -1;
+    }
+};
