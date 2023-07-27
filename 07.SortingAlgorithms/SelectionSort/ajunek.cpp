8b7bf87f6f7b71e9b05617d5893724d3e607c060
Amol Shinde <shindeamol409@gmail.com>
2023-07-27 15:07:26 +0530

    Added new files.

--- 07.SortingAlgorithms/SelectionSort/ajunek.cpp
+++ 07.SortingAlgorithms/SelectionSort/ajunek.cpp
@@ -1,3 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void selection_sort(int arr[], int n) {
+    for (int i = 0; i <= n-1; i++) {
+        int mini = i;
+        for (int j = i; j <= n; j++) {
+            if(arr[j] < arr[mini]) {
+                mini = j;
+            }
+        }
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp;
+    }
+}
+
 int main() {
     int n;
     cin >> n;
