d1bd77f359411a5693b8f3dcf97cad1274f60750
Amol Shinde <shindeamol409@gmail.com>
2023-07-30 16:10:41 +0530

    Added new files.

--- 07.SortingAlgorithms/BubbleSort/main.cpp
+++ 07.SortingAlgorithms/BubbleSort/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
 
+// Function to perform bubble sort on an array
 void bubble_sort(int arr[], int n) {
+    // Outer loop for the passes
     for (int i = n - 1; i >= 0; i--) {
+        // Inner loop for the comparisons and swapping
         for (int j = 0; j <= i - 1; j++) {
+            // If the current element is greater than the next element, swap them
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j + 1];
                 arr[j + 1] = arr[j];
@@ -13,15 +17,28 @@ void bubble_sort(int arr[], int n) {
     }
 }
 
+// Main function where the program execution starts
 int main() {
+    // Declaring and taking input for the number of elements in the array
     int n;
     cin >> n;
+
+    // Declaring an array of size n
     int arr[n];
-    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    // Taking input for the elements of the array
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    // Calling the bubble_sort function to sort the array
     bubble_sort(arr, n);
+
+    // Printing the sorted array
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
+
+    // Indicating successful program execution
     return 0;
 } 
 
