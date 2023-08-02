e9c6eca927eeb80cde7c62bcfb906879b421e51b
Amol Shinde <shindeamol409@gmail.com>
2023-08-02 16:56:22 +0530

    Added new files.

--- 03.SolvingLeetcodeProblems/KthLargestElementInAnArray/main.cpp
+++ 03.SolvingLeetcodeProblems/KthLargestElementInAnArray/main.cpp
@@ -141,3 +141,45 @@ private:
 I suggest you to read the Heapsort chapter of Introduction to Algorithms if you are not familiar with it. 
 The following code implements a max-heap. */
 
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        buildMaxHeap(nums);
+        for (int i = 0; i < k - 1; i++) {
+            swap(nums[0], nums[--heapSize]);
+            maxHeapify(nums, 0);
+        }
+        return nums[0];
+    }
+private:
+    int heapSize;
+    
+    int left(int i) {
+        return (i << 1) + 1;
+    }
+    
+    int right(int i) {
+        return (i << 1) + 2;
+    }
+    
+    void maxHeapify(vector<int>& nums, int i) {
+        int largest = i, l = left(i), r = right(i);
+        if (l < heapSize && nums[l] > nums[largest]) {
+            largest = l;
+        }
+        if (r < heapSize && nums[r] > nums[largest]) {
+            largest = r;
+        }
+        if (largest != i) {
+            swap(nums[i], nums[largest]);
+            maxHeapify(nums, largest);
+        }
+    }
+    
+    void buildMaxHeap(vector<int>& nums) {
+        heapSize = nums.size();
+        for (int i = (heapSize >> 1) - 1; i >= 0; i--) {
+            maxHeapify(nums, i);
+        }
+    }
+};
