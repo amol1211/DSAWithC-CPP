788b58d90be1865725710cd0df8c49b5e529cfd0
Amol Shinde <shindeamol409@gmail.com>
2023-08-09 17:16:34 +0530

    Added new files.

---
+++ 03.SolvingLeetcodeProblems/FindFirstAndLastPositionOfElementInSortedArray/ajunek.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    public:
+       vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans;
+        int start = -1, end = -1;
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (nums[mid] == target) {
+                int temp = mid;
+                while (mid > 0 && nums[mid - 1] == target) 
+                mid--;
+                start = mid;
+                while(temp < nums.size() - 1 && nums[temp + 1] == target)
+                temp++;
+                end = temp;
+
+                ans.push_back(start);
+                ans.push_back(end);
+                return ans;
+            }
+            else if(nums[mid] > target)
+                j = mid - 1;
+            else 
+                i = mid + 1;
+        }
+
+            ans.push_back(-1);
+            ans.push_back(-1);
+            return ans;
+    }
+};
