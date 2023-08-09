ae1ae73d82066c93d12e771c568f6826a9a498c1
Amol Shinde <shindeamol409@gmail.com>
2023-08-09 17:25:09 +0530

    Added new files.

--- 03.SolvingLeetcodeProblems/FindFirstAndLastPositionOfElementInSortedArray/ajunek.cpp
+++ 03.SolvingLeetcodeProblems/FindFirstAndLastPositionOfElementInSortedArray/ajunek.cpp
@@ -84,3 +84,27 @@ public:
         return ans;
     }
 };
+
+/*------------------------------------------------------------------------------------*/
+
+//Using inbuilt lower and upper bound functions
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int>ans;
+        vector<int>::iterator it, lower, upper;
+        it = find(nums.begin(), nums.end(), target);
+        if(it != nums.end()){
+            lower = lower_bound(nums.begin(), nums.end(), target);
+            upper = upper_bound(nums.begin(), nums.end(), target);
+            ans.push_back(lower - nums.begin());
+            ans.push_back(upper - nums.begin() - 1);
+        }
+        else{
+            ans.push_back(-1);
+            ans.push_back(-1);
+        }
+        return ans;
+    }
+};
