3b960310945760618f8029678ad26ae1a264a834
Amol Shinde <shindeamol409@gmail.com>
2023-08-18 03:29:02 +0530

    Added new file.

--- 06.LinkedList/InsertANodeAtNthPosition/ajunek.cpp
+++ 06.LinkedList/InsertANodeAtNthPosition/ajunek.cpp
@@ -22,19 +22,20 @@ void Insert(int data, int n) {
     temp1->data = data;
     temp1->next = nullptr;
 
-    if(n == 1) {
+    if (n == 1) {
         temp1->next = head;
         head = temp1;
         return;
     }
 
     Node* temp2 = head;
-    for (int i = 0; i < n-2; i++) {
+    for (int i = 0; i < n - 2; i++) {
         temp2 = temp2->next;
     }
+
     temp1->next = temp2->next;
     temp2->next = temp1;
-}
+} 
 
 int main() {
     Node* head = nullptr;
