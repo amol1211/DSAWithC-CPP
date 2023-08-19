a48c7f7af960e43e8700c5c3688133e9384486d4
Amol Shinde <shindeamol409@gmail.com>
2023-08-19 17:19:21 +0530

    Modified file.

--- 03.SolvingLeetcodeProblems/MoveZeroes/ajunek.cpp
+++ 03.SolvingLeetcodeProblems/MoveZeroes/ajunek.cpp
@@ -14,3 +14,20 @@ class Solution {
             }
         }
 }
+
+/*-------------------------------------------------------*/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int nextNonZero = 0; // Position to place the next non-zero element
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                swap(nums[nextNonZero], nums[i]);
+                nextNonZero++;
+            }
+        }
+    }
+};
