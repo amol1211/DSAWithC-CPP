27b1eb14de80df6278cfc53b05cb1684fee8b231
Amol Shinde <shindeamol409@gmail.com>
2023-08-22 11:00:06 +0530

    Deleted ajunek.exe

--- 06.LinkedList/DeleteANodeAtNthPosition/ajunek.cpp
+++ 06.LinkedList/DeleteANodeAtNthPosition/ajunek.cpp
@@ -19,56 +19,52 @@ void Insert(int x) {
     }
 
     Node* last = head;
-
-    while (last->next != nullptr) {
+    while(last->next != nullptr) {
         last = last->next;
     }
 
     last->next = temp;
 }
-    void Print() {
-        Node* temp = head;
-        while (temp != nullptr) {
-            cout << temp->data << " ";
-            temp = temp->next;
-        }
-        cout << '\n';
+
+void Print() {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << '\n';
+}
+
+void Delete(int n) {
+    Node* temp1 = head;
+
+    if (n == 1) {
+        head = temp1->next;
+        delete(temp1);
+        return;
     }
 
-    void Delete(int n) {
-        Node* temp1 = head;
-
-        if (n == 1) {
-            head = temp1->next;
-            delete(temp1);
-            return;
-        }
-
-        for (int i = 0; i < n - 2; i++) {
-            temp1 = temp1->next;
-        }
-
-        Node* temp2 = temp1->next;
-        temp1->next = temp2->next;
-        delete(temp2);
+    for (int i = 0; i < n - 2; i++) {
+        temp1 = temp1->next;
     }
+    Node* temp2 = temp1->next;
+    temp1->next = temp2->next;
+    delete(temp2);
+}
 
-    int main() {
-        head = nullptr;
-        Insert(2);
-        Insert(4);
-        Insert(6);
-        Insert(5);
-        Print();
+int main() {
+    head = nullptr;
+    Insert(2);
+    Insert(4);
+    Insert(6);
+    Insert(5);
+    Print();
 
-        int n;
-        cout << "Enter the position you want to delete the data value of: ";
-        cin >> n;
-        Delete(n);
-        Print();
+    int n;
+    cout << "Enter the position you want to delete the data value of: ";
+    cin >> n;
+    Delete(n);
+    Print();
 
-        return 0;
-    
-    }
-
-
+    return 0;
+}
