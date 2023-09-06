652b98a323cadc9f3a31f96ab47cff82a60bb5e4
Amol Shinde <shindeamol409@gmail.com>
2023-09-06 10:05:04 +0530

    Added new file.

---
+++ 07.BabbarLinkedList/InsertAtHead/ajunek.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this -> data = data;
+        this -> next = nullptr;
+    }
+};
+
+void insertAtHead(Node* &head, int data) {
+    Node* temp = new Node(data);
+    temp->next = head;
+    head = temp;
+}
+
+void print(Node* &head) {
+    Node* temp = head;
+    while(temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << '\n';
+}
+
+int main() {
+    Node* node1 = new Node(10);
+    Node* head = node1;
+    print(head);
+
+    insertAtHead(head, 12);
+    print(head);
+
+    insertAtHead(head, 15);
+    print(head);
+
+    return 0;
+}
