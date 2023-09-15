b169a149691cbeec4a705575ddf6766c07900df4
Amol Shinde <shindeamol409@gmail.com>
2023-09-15 16:39:45 +0530

    Added new file.

--- 03.SolvingLeetcodeProblems/LinkedList/LinkedListCycle/ajunek.cpp
+++ 03.SolvingLeetcodeProblems/LinkedList/LinkedListCycle/ajunek.cpp
@@ -1,4 +1,23 @@
+// 141. Linked List Cycle
 
+// Approach 1: Hash Table
+
+class Solution{
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode *> visited_nodes;
+        ListNode *current_node = head;
+
+        while (current_node != nullptr) {
+            if (visited_nodes.find(current_node) != visited_nodes.end()) {
+                return true;
+            }
+            visited_nodes.insert(current_node);
+            current_node = current_node->next;
+        }
+        return false;
+    }
+};
 
 /*------------------------------------------------------------------*/
 
