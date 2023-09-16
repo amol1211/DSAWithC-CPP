ae5c6ef3f7b5e7b01caef7af317315622280c186
Amol Shinde <shindeamol409@gmail.com>
2023-09-16 18:49:33 +0530

    Added new file.

--- 03.SolvingLeetcodeProblems/LinkedList/MiddleOfTheLinkedList/main.cpp
+++ 03.SolvingLeetcodeProblems/LinkedList/MiddleOfTheLinkedList/main.cpp
@@ -27,3 +27,21 @@ public:
 
 /*--------------------------------------------------------------------------*/
 
+//APPROACH-2
+//Keep 2 pointers, slow and fast.
+//slow moves by one step whereas fast moves by 2 steps.
+//By the time fast reaches the end of the LL, slow will point to the middle.
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow= slow->next;
+            fast= fast->next->next;
+        }
+        return slow;
+    }
+};
