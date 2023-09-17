827a7b3417ab440672e3012699f74dd611f2c5e1
Amol Shinde <shindeamol409@gmail.com>
2023-09-17 20:06:51 +0530

    Added new file.

---
+++ 03.SolvingLeetcodeProblems/LinkedList/MiddleOfTheLinkedList/ajunek.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+    public:
+        ListNode* middleNode(ListNode* head) {
+            ListNode *temp = head;
+            int length = 0;
+            while(temp) {
+                length++;
+                temp = temp->next;
+            }
+            int half = length/2;
+            temp = head;
+            while(temp && half--) {
+                temp = temp->next;
+            }
+            return temp;
+        }
+};
+
