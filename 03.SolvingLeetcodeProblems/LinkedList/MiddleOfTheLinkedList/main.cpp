24a7f94703062b678aba40ad4fea3aec797a0c0f
Amol Shinde <shindeamol409@gmail.com>
2023-09-19 21:44:44 +0530

    Added new file.

--- 03.SolvingLeetcodeProblems/LinkedList/MiddleOfTheLinkedList/main.cpp
+++ 03.SolvingLeetcodeProblems/LinkedList/MiddleOfTheLinkedList/main.cpp
@@ -5,7 +5,25 @@
 //Find the middle in the second traversal (middle is length/2)
 //Return pointer pointing to the middle element.
 
-
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *temp = head;
+        int length=0;
+        while(temp)
+        {
+            length++;
+            temp = temp->next;
+        }
+        int half = length/2;
+        temp=head;
+        while(temp && half--)
+        {
+            temp=temp->next;
+        }
+        return temp;
+    }
+};
 
 /*--------------------------------------------------------------------------*/
 
