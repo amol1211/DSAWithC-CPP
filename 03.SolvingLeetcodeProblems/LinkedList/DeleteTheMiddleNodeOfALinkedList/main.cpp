5b269bf427d4eec5a21384e1eb5ac97ce1a8e622
Amol Shinde <shindeamol409@gmail.com>
2023-10-05 10:56:27 +0530

    Added new file.

--- 03.SolvingLeetcodeProblems/LinkedList/DeleteTheMiddleNodeOfALinkedList/main.cpp
+++ 03.SolvingLeetcodeProblems/LinkedList/DeleteTheMiddleNodeOfALinkedList/main.cpp
@@ -1,5 +1,7 @@
 //2095. Delete the Middle Node of a Linked List
 
+////Approach 1
+
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
@@ -31,6 +33,34 @@ public:
 
         prev->next = curr->next;
         delete curr;
+
+        return head;
+    }
+};
+
+/*--------------------------------------------------------*/
+
+//Approach 2
+
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return nullptr;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        prev->next = slow->next;
+        delete slow;
 
         return head;
     }
