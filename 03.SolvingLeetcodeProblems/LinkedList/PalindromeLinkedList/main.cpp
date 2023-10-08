19fb5aeeacfecb66eb9774ade6b7dad64fea044c
Amol Shinde <shindeamol409@gmail.com>
2023-10-08 13:06:18 +0530

    Added new file.

--- 03.SolvingLeetcodeProblems/LinkedList/PalindromeLinkedList/main.cpp
+++ 03.SolvingLeetcodeProblems/LinkedList/PalindromeLinkedList/main.cpp
@@ -42,3 +42,73 @@ public:
 //Space complexity : O(n)
 
 /*----------------------------------------------------------*/
+
+//Approach 2
+
+class Solution {
+private:
+    ListNode* getMid(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+
+    ListNode* reverse(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        ListNode* next = nullptr;
+
+        while (curr != nullptr) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+public:
+    bool isPalindrome(ListNode* head) {
+        if (head->next == nullptr) {
+            return true;
+        }
+
+        // Step 1: Find middle
+        ListNode* middle = getMid(head);
+
+        // Step 2: Reverse list after middle node
+        ListNode* temp = middle->next;
+        middle->next = reverse(temp);
+
+        // Step 3: Compare both halves
+        ListNode* head1 = head;
+        ListNode* head2 = middle->next;
+
+        while (head2 != nullptr) {
+            if (head1->val != head2->val) {
+                return false;
+            }
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+
+        // Step 4: Make the list as it was given (optional)
+        middle->next = reverse(middle->next);
+
+        return true;
+    }
+};
+
+//Time complexity : O(n)
+//Space complexity : O(1)
+
+
+
+
+
+
