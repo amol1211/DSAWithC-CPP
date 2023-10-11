3583179d5b5652bf73e9d89ed4f859083a1d335e
Amol Shinde <shindeamol409@gmail.com>
2023-10-11 08:43:21 +0530

    Added new files.

--- 03.SolvingLeetcodeProblems/LinkedList/CopyListWithRandomPointer/ajunek.cpp
+++ 03.SolvingLeetcodeProblems/LinkedList/CopyListWithRandomPointer/ajunek.cpp
@@ -50,3 +50,44 @@ public :
         return clonehead;
     }
 };
+
+/*-------------------------------------------------------------------------*/
+
+class Solution {
+    public:
+       Node* copyRandomList(Node* head) {
+        if (!head) return nullptr;
+
+        Node* current = head;
+        while (curr != nullptr) {
+            Node* copy = new Node(current->val);
+            copy->next = curr->next;
+            curr->next = copy;
+            curr = copy->next;
+        }
+
+        current = head;
+        while (current != nullptr) {
+            if (current->random != nullptr) {
+                current->next->random = current->random->next;
+            }
+            current = current->next->next;
+        }
+
+        current = head;
+        Node* copiedHead = current->next;
+        Node* copiedCurrent = copiedHead;
+
+        while (current != nullptr) {
+            current->next = copiedCurrent->next;
+            current = current->next;
+
+            if (current != nullptr) {
+            copiedCurrent->next = current->next;
+            copiedCurrent = copiedCurrent->next;
+        }
+        }
+
+        return copiedHead;      
+    }
+};
