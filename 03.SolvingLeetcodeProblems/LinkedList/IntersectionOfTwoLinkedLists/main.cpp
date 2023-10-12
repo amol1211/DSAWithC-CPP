890076155ab5fc8ba8f208a53550c32775d83bad
Amol Shinde <shindeamol409@gmail.com>
2023-10-12 22:53:00 +0530

    Added new file.

--- 03.SolvingLeetcodeProblems/LinkedList/IntersectionOfTwoLinkedLists/main.cpp
+++ 03.SolvingLeetcodeProblems/LinkedList/IntersectionOfTwoLinkedLists/main.cpp
@@ -42,3 +42,21 @@ public:
 
 //Time Complexity: O(N) 
 //Space Complexity: O(1)
+
+/*------------------------------------------------*/
+
+//GFG problem
+
+//Function to find intersection point in Y shaped Linked Lists.
+int intersectPoint(Node* head1, Node* head2) {
+    Node* temp1 = head1;
+    Node* temp2 = head2;
+
+    while (temp1 != temp2) {
+        // Move both pointers forward
+        temp1 = (temp1 == nullptr) ? head2 : temp1->next;
+        temp2 = (temp2 == nullptr) ? head1 : temp2->next;
+    }
+
+    return (temp1 == nullptr) ? -1 : temp1->data; // Return the intersection point's data
+}
---
+++ 03.SolvingLeetcodeProblems/LinkedList/RemoveNodesFromLinkedList/main.cpp
@@ -0,0 +1,24 @@
+//2487. Remove Nodes From Linked List
+
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        // Base case: if the list is empty or has only one node, return it as is.
+        if (!head || !head->next) return head;
+
+        // Recursively call removeNodes on the rest of the list.
+        ListNode* temp = removeNodes(head->next);
+
+        // If the value of the node to the right (temp) is greater than the current node (head),
+        // return the rest of the modified list (temp).
+        if (temp->val > head->val) return temp;
+
+        // If the value of the current node (head) is greater than or equal to the next node (temp),
+        // set the next node of the current node to be the result of the recursive call (temp).
+        head->next = temp;
+
+        // Return the current node, which now potentially has nodes with greater values removed.
+        return head;
+    }
+};
+
