715027165cf53bc9684daa3b8fa91577f617c960
Amol Shinde <shindeamol409@gmail.com>
2023-10-23 19:00:33 +0530

    Added new file.

---
+++ 03.SolvingLeetcodeProblems/Stack/InsertElementAtBottomInGivenStack/main.cpp
@@ -0,0 +1,30 @@
+//GFG Problem
+
+class Solution {
+public:
+    // Function to move elements to insert 'x' at the bottom of the stack.
+    void solve(stack<int> &s, int x) {
+        // Base case - if the stack is empty.
+        if (s.empty()) {
+            s.push(x);  // Insert 'x' at the bottom (since the stack is empty).
+            return;
+        }
+
+        int num = s.top(); // Store the top element of the stack.
+        s.pop();           // Remove the top element from the stack.
+
+        // Recursive call - move to the bottom.
+        solve(s, x);
+
+        s.push(num); // Push the stored element back on top of 'x'.
+    }
+
+    // Function to insert an element 'x' at the bottom of the stack.
+    stack<int> insertAtBottom(stack<int> myStack, int x) {
+        solve(myStack, x); // Call the solve function to insert 'x' at the bottom.
+        return myStack;    // Return the modified stack with 'x' at the bottom.
+    }
+};
+
+//Time Complexity: O(N)
+//Space Complexity: O(N)
