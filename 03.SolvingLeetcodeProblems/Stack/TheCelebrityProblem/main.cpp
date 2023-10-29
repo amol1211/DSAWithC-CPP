a43647d813f1f530cec0a87ea390c98f9ea5a35f
Amol Shinde <shindeamol409@gmail.com>
2023-10-29 16:19:31 +0530

    Added new file.

---
+++ 03.SolvingLeetcodeProblems/Stack/TheCelebrityProblem/main.cpp
@@ -0,0 +1,86 @@
+//GFG problem
+
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+//Stack approach 1
+
+class Solution 
+{
+private: 
+    // Helper function to check if a knows b.
+    bool knows(vector<vector<int> >& M, int a, int b, int n) {
+        if (M[a][b] == 1)
+            return true;
+        else
+            return false;
+    }
+    
+public:
+    // Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) {
+        stack<int> s;
+        // Step 1: Push all elements into the stack
+        for (int i = 0; i < n; i++) {
+            s.push(i);
+        }
+        
+        // Step 2: Get 2 elements and compare them
+        while (s.size() > 1) {
+            int a = s.top();
+            s.pop();
+            
+            int b = s.top();
+            s.pop();
+            
+            if (knows(M, a, b, n)) {
+                s.push(b);
+            }
+            else {
+                s.push(a);
+            }
+        }
+        
+        int ans = s.top();
+        // Step 3: Single element in the stack could be a potential celebrity, so, we have to verify it.
+        bool rawCheck = false;
+        int zeroCount = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (M[ans][i] == 0) {
+                zeroCount++;
+            }
+        }
+        
+        // Checking if all zeroes
+        if (zeroCount == n) {
+            rawCheck = true;
+        }
+
+        // Column check
+        bool columnCheck = false;
+        int oneCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (M[i][ans] == 1) {
+                oneCount++;
+            }
+        }
+
+        if (oneCount == n - 1) {
+            columnCheck = true;
+        }
+
+        if (rawCheck == true && columnCheck == true) {
+            return ans; // The celebrity was found.
+        }
+        else {
+            return -1; // No celebrity was found.
+        }
+    }
+};
+
+//Time Complexity: O(n)
+//Space Complexity: O(n)
