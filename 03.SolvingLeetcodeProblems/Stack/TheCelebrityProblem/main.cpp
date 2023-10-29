0062e848542f6af42376718dcdb1f9aea4b3c6ee
Amol Shinde <shindeamol409@gmail.com>
2023-10-29 16:21:39 +0530

    Added new file.

--- 03.SolvingLeetcodeProblems/Stack/TheCelebrityProblem/main.cpp
+++ 03.SolvingLeetcodeProblems/Stack/TheCelebrityProblem/main.cpp
@@ -82,5 +82,76 @@ public:
     }
 };
 
+/*--------------------------------------------------------------------------*/
+
+class Solution 
+{
+private: 
+    bool knows(vector<vector<int> >& M, int a, int b, int n) {
+        if (M[a][b] == 1)
+            return true;
+        else
+            return false;
+    }
+    
+public:
+    // Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) {
+        stack<int> s;
+        // Step 1: Push all elements into the stack
+        for (int i = 0; i < n; i++) {
+            s.push(i);
+        }
+        
+        // Step 2: Get 2 elements and compare them
+        while (s.size() > 1) {
+            int a = s.top();
+            s.pop();
+            
+            int b = s.top();
+            s.pop();
+            
+            if (knows(M, a, b, n)) {
+                s.push(b);
+            }
+            else {
+                s.push(a);
+            }
+        }
+        
+        int ans = s.top();
+        // Step 3: Single element in the stack could be a potential celebrity, so, we have to verify it.
+        
+        int zeroCount = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (M[ans][i] == 0) {
+                zeroCount++;
+            }
+        }
+        
+        //Checking if all zeroes
+        if (zeroCount != n) {
+            return -1;
+        }
+
+        // Column check
+        int oneCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (M[i][ans] == 1) {
+                oneCount++;
+            }
+        }
+
+        if (oneCount != n - 1) {
+            return -1;
+        }
+
+        
+        return ans;
+    }
+};
+
 //Time Complexity: O(n)
 //Space Complexity: O(n)
