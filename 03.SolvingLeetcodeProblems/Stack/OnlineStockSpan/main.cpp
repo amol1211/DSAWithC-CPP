8afd238891558ee9183755a815e57b49bf527b6f
Amol Shinde <shindeamol409@gmail.com>
2023-11-10 00:46:57 +0530

    Added new file.

--- 03.SolvingLeetcodeProblems/Stack/OnlineStockSpan/main.cpp
+++ 03.SolvingLeetcodeProblems/Stack/OnlineStockSpan/main.cpp
@@ -1,5 +1,33 @@
 // 901. Online Stock Span
 
+// 1. Brute force approach
+
+class StockSpanner {
+public:
+    vector<int> prices; // Vector to store historical prices
+
+    StockSpanner() {
+    }
+
+    int next(int price) {
+        prices.push_back(price); // Add the current price to the prices vector
+        int span = 1; // Initialize the span to 1
+        int i = prices.size() - 2; // Start iterating from the second-to-last element in prices
+
+        while (i >= 0 && prices[i] <= price) {
+            span++; // Increment the span for each smaller or equal price to the left
+            i--; // Move to the left in the prices vector
+        }
+
+        return span; // Return the calculated span
+    }
+};
+
+//Time complexity: O(N) 
+//Space complexity: O(N)
+
+/*-------------------------------------------------------------------*/
+
 // 2. Stack approach 
 
 class StockSpanner {
