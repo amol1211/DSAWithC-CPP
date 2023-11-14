38f7aadb8bb316160129dfb008ee5626de56ba87
Amol Shinde <shindeamol409@gmail.com>
2023-11-14 23:27:40 +0530

    Added new file.

--- 03.SolvingLeetcodeProblems/Stack/SumOfSubarrayRanges/main.cpp
+++ 03.SolvingLeetcodeProblems/Stack/SumOfSubarrayRanges/main.cpp
@@ -36,3 +36,58 @@ public:
         return sum;
     }
 };
+
+//Time complexity : O(n^2)
+//Space complexity : O(1)
+
+/*---------------------------------------------------------------------------------*/
+
+class Solution {
+public:
+    // Function to calculate the sum of products of subarray ranges based on the input vector 'nums'.
+    long subArrayRanges(vector<int>& nums) {
+        // Variable to store the final result.
+        long result = 0;
+        
+        // Stack to track increasing elements' indices.
+        stack<int> inc;
+        
+        // Stack to track decreasing elements' indices.
+        stack<int> dec;
+
+        // Iterate through the vector 'nums' and one additional iteration for handling the remaining elements.
+        for (int i = 0; i <= nums.size(); ++i) {
+            // Process increasing elements.
+            while (!inc.empty() && (i == nums.size() || nums[inc.top()] < nums[i])) {
+                int currIndex = inc.top();
+                inc.pop();
+                int leftBound = inc.empty() ? -1 : inc.top();
+                int rightBound = i;
+                
+                // Update the result by adding the product of the current subarray range.
+                result += (currIndex - leftBound) * (rightBound - currIndex) * static_cast<long>(nums[currIndex]);
+            }
+            
+            // Process decreasing elements.
+            while (!dec.empty() && (i == nums.size() || nums[dec.top()] > nums[i])) {
+                int currIndex = dec.top();
+                dec.pop();
+                int leftBound = dec.empty() ? -1 : dec.top();
+                int rightBound = i;
+                
+                // Update the result by subtracting the product of the current subarray range.
+                result -= (currIndex - leftBound) * (rightBound - currIndex) * static_cast<long>(nums[currIndex]);
+            }
+            
+            // Push the current index onto both increasing and decreasing stacks.
+            inc.push(i);
+            dec.push(i);
+        }
+
+        // Return the final result.
+        return result;
+    }
+};
+
+//Time complexity : O(n)
+//Space complexity : O(n)
