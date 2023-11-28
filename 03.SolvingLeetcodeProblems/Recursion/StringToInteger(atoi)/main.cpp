3772b52e43dc2a425c4148bb6b561e24d1032055
Amol Shinde <shindeamol409@gmail.com>
2023-11-28 19:31:47 +0530

    Added new file.

--- 03.SolvingLeetcodeProblems/Recursion/StringToInteger(atoi)/main.cpp
+++ 03.SolvingLeetcodeProblems/Recursion/StringToInteger(atoi)/main.cpp
@@ -11,34 +11,41 @@ using namespace std;
 class Solution {
 public:
     int myAtoi(string s) {
+        // Get the length of the input string
+        int n = s.length();
+        // Initialize the index i, sign, and result variables
         int i = 0;
         int sign = 1;
         long result = 0;
 
-        // Step 1: Ignore leading whitespace
-        while (i < s.length() && s[i] == ' ') {
+        // Skip leading whitespaces
+        while (i < n && s[i] == ' ') {
             i++;
         }
 
-        // Step 2: Check for sign
-        if (i < s.length() && (s[i] == '+' || s[i] == '-')) {
-            sign = (s[i++] == '-') ? -1 : 1;
+        // Handle sign only once
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            // Update sign based on the encountered sign character
+            sign = (s[i] == '-') ? -1 : 1;
+            i++; // Move to the next character
         }
 
-        // Step 3: Read in digits
-        while (i < s.length() && isdigit(s[i])) {
-            result = result * 10 + (s[i++] - '0');
+        // Process digits
+        while (i < n && isdigit(s[i])) {
+            // Update the result by appending the current digit
+            result = result * 10 + s[i] - '0';
 
-            // Step 4: Check for overflow
+            // Check for overflow
             if (result * sign > INT_MAX) {
-                return INT_MAX;
+                return INT_MAX; // Return maximum integer value in case of overflow
+            } else if (result * sign < INT_MIN) {
+                return INT_MIN; // Return minimum integer value in case of overflow
             }
-            else if (result * sign < INT_MIN) {
-                return INT_MIN;
-            }
+
+            i++; // Move to the next character
         }
 
-        // Apply sign
+        // Return the final result after applying the sign
         return static_cast<int>(result * sign);
     }
 };
