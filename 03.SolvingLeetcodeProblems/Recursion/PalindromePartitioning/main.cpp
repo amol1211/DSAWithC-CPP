d90606871aa1f1c51261a55730a8f59542f58465
Amol Shinde <shindeamol409@gmail.com>
2023-11-30 11:31:43 +0530

    Added new file.

--- 03.SolvingLeetcodeProblems/Recursion/PalindromePartitioning/main.cpp
+++ 03.SolvingLeetcodeProblems/Recursion/PalindromePartitioning/main.cpp
@@ -9,23 +9,24 @@ using namespace std;
 
 class Solution {
 private:
-    // Declare a variable to store the length of the input string
+    // Variable to store the length of the input string
     int n;
 
-    // Function to check if a substring is a palindrome
+    // Helper function to check if a substring is a palindrome
     bool isPalindrome(string &s, int l, int r) {
-        // Check if the substring is a palindrome using two pointers
+        // Check if the characters at the given indices are equal
         while (l < r) {
             if (s[l] != s[r]) 
                 return false;
+            // Move towards the center of the substring
             l++;
             r--;     
         }
-        // Return true if the substring is a palindrome
+        // If the loop completes, the substring is a palindrome
         return true;
     } 
 
-    // Backtracking function to generate all possible palindrome partitions
+    // Backtracking function to find all possible palindromic partitions
     void backtrack(string &s, int index, vector<string> curr, vector<vector<string>> &result) {
         // If we have reached the end of the string, add the current partition to the result
         if (index == n) {
@@ -33,36 +34,37 @@ private:
             return;
         }
 
-        // Iterate through the string and generate partitions
+        // Explore all possible partitions starting from the current index
         for (int i = index; i < n; i++) {
-            // If the substring from index to i is a palindrome
+            // If the substring is a palindrome, add it to the current partition
             if (isPalindrome(s, index, i)) {
-                // Add the palindrome substring to the current partition
                 curr.push_back(s.substr(index, i - index + 1));
-                // Recursively generate partitions for the remaining substring
+                // Recursively move to the next index
                 backtrack(s, i + 1, curr, result);
-                // Backtrack: Remove the last added substring for the next iteration
+                // Backtrack: remove the last added substring to explore other possibilities
                 curr.pop_back();
             }
         }
     } 
 
 public:
-    // Main function to generate all possible palindrome partitions for the input string
+    // Main function to get all palindromic partitions of a string
     vector<vector<string>> partition(string s) {
-        // Set the length of the input string
+        // Initialize the length of the string
         n = s.length();
-        // Initialize vectors to store the result and the current partition
+        // Vector to store the final result
         vector<vector<string>> result;
+        // Vector to store the current partition during the backtracking process
         vector<string> curr;
 
-        // Start backtracking from index 0
+        // Start the backtracking process from the beginning of the string
         backtrack(s, 0, curr, result);
 
-        // Return the final result containing all palindrome partitions
+        // Return the final result containing all palindromic partitions
         return result;
     }
 };
+
 
 //Time complexity is exponential, O(2^n), where n is the length of the input string. 
 //Space complexity : O(n)
