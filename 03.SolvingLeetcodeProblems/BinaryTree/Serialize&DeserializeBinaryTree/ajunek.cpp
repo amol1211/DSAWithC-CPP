28cd1212ca1211a769af6277c694158928f59441
Amol Shinde <shindeamol409@gmail.com>
2023-12-16 23:12:27 +0530

    Added new file.

--- 03.SolvingLeetcodeProblems/BinaryTree/Serialize&DeserializeBinaryTree/ajunek.cpp
+++ 03.SolvingLeetcodeProblems/BinaryTree/Serialize&DeserializeBinaryTree/ajunek.cpp
@@ -1,4 +1,3 @@
-//297. Serialize and Deserialize Binary Tree
 
 #include <iostream>
 #include <queue>
@@ -8,57 +7,44 @@ using namespace std;
 
 struct TreeNode {
     int val;
-        TreeNode *left;
-        TreeNode *right;
-       TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- };
-
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
 class Codec {
 private:
-    // Helper function for deserialization
-    TreeNode* deserializeHelper(queue<string>& nodes) {
+    TreeNode* deserializeHelper(queue<string> &nodes) {
         string nodeVal = nodes.front();
         nodes.pop();
 
         if (nodeVal == "null") {
-            return nullptr; // Return nullptr for "null" nodes
+            return nullptr;
         }
 
-        // Create a new node with the value
         TreeNode* root = new TreeNode(stoi(nodeVal));
-        // Recursively deserialize the left and right subtrees
         root->left = deserializeHelper(nodes);
         root->right = deserializeHelper(nodes);
 
         return root;
-    }
-
+    }    
 public:
-    // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
         if (!root) {
-            return "null"; // Return "null" for empty nodes
+            return "null";
         }
-
-        // Preorder traversal for serialization
         return to_string(root->val) + "," + serialize(root->left) + "," + serialize(root->right);
     }
-    
 
-    // Decodes your encoded data to tree.
+
+
     TreeNode* deserialize(string data) {
         queue<string> nodes;
         stringstream ss(data);
         string node;
-
-        // Tokenize the serialized string
+        
         while (getline(ss, node, ',')) {
             nodes.push(node);
         }
-
         return deserializeHelper(nodes);
     }
 };
-
-// Time complexity : O(n)
-// Space complexity : O(n)
