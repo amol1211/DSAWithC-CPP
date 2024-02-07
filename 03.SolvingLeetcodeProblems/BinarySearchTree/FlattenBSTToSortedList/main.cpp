f29c3e0aded6e41fbe3189e28e8ee388f716f6ca
Amol Shinde <shindeamol409@gmail.com>
2024-02-07 22:30:27 +0530

    Added new file.

--- 03.SolvingLeetcodeProblems/BinarySearchTree/FlattenBSTToSortedList/main.cpp
+++ 03.SolvingLeetcodeProblems/BinarySearchTree/FlattenBSTToSortedList/main.cpp
@@ -18,6 +18,7 @@ public:
     }
 };
 
+//Approach 1 : Vector-Based In-order Traversal Approach
 
 class Solution
 {
@@ -71,3 +72,50 @@ time since we iterate through each value once.
 Space Complexity:
 
 The space complexity is O(n) due to the storage of in-order traversal values in the vector.*/
+
+/*------------------------------------------------------------------------------------------------*/
+
+//Approach 2 :  Recursive In-order Traversal / Pointer Modification In-order Traversal approach
+
+class Solution
+{
+private:
+    // Function to perform in-order traversal and flatten the binary search tree into a linked list
+    void inOrder(Node* root, Node* &prev) {
+        if (!root) return; // Base case: if root is null, return
+        
+        inOrder(root->left, prev); // Recursively traverse the left subtree
+        
+        prev->left = nullptr; // Set the left child of the previous node to null
+        prev->right = root; // Set the right child of the previous node to the current node
+        prev = root; // Update the previous node to the current node
+        
+        inOrder(root->right, prev); // Recursively traverse the right subtree
+    }
+
+public:
+    // Function to flatten a binary search tree into a linked list
+    Node *flattenBST(Node *root)
+    {
+        Node* dummy = new Node(-1); // Create a dummy node with value -1
+        Node* prev = dummy; // Initialize a pointer to the previous node, starting with the dummy node
+        
+        inOrder(root, prev); // Call the in-order traversal function to flatten the tree
+        
+        prev->left = nullptr; // Set the left child of the last node to null
+        prev->right = nullptr; // Set the right child of the last node to null
+        
+        return dummy->right; // Return the head of the flattened linked list
+    }
+};
+
+/*Time Complexity:
+
+The time complexity of this algorithm is O(n), where n is the number of nodes in the binary 
+search tree. This is because we visit each node exactly once during the in-order traversal.
+
+Space Complexity:
+
+The space complexity is O(1) because we are not using any additional data structures that 
+grow with the size of the input. We are only using a constant amount of space for storing 
+pointers and variables.*/
